700dea1dde4875f69fffe45ea6d34f346a72fdd7
MTHSalge <maathsalge@hotmail.com>
2022-07-08 19:57:06 -0300

    Adicao aula de modulos

---
+++ Aulas/JavaScript/12-main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./12-funcoes.mjs";
+
+console.log(mostraIdade("Matheus Salge", 26));
+console.log(mostraIdade("Matheus Salge", "Santos"));
+console.log(mostraIdade("Matheus Salge", "Jogar Jogos"));
