81964c985903863de6fa89d4f399599d05ef5a52
MTHSalge <maathsalge@hotmail.com>
2022-07-19 16:37:52 -0300

    adicao aula introdutoria angular

---
+++ Aulas/Angular/course-manager/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, Pipe } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    CoreModule,
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      }
+    ])
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
